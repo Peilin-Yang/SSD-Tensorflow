2350a064fae08192c41145ce94e0ae371bbdbc32
Peilin Yang <yangpeilyn@gmail.com>
2017-02-24 07:12:42 -0500

    add bib

--- train_ssd_network.py
+++ train_ssd_network.py
@@ -114,9 +114,9 @@ tf.app.flags.DEFINE_float(
 # Dataset Flags.
 # =========================================================================== #
 tf.app.flags.DEFINE_string(
-    'dataset_name', 'imagenet', 'The name of the dataset to load.')
+    'dataset_name', 'bib', 'The name of the dataset to load.')
 tf.app.flags.DEFINE_integer(
-    'num_classes', 21, 'Number of classes to use in the dataset.')
+    'num_classes', 2, 'Number of classes to use in the dataset.')
 tf.app.flags.DEFINE_string(
     'dataset_split_name', 'train', 'The name of the train/test split.')
 tf.app.flags.DEFINE_string(
